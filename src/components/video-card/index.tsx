1344d8802a2d4ecc5dc5b5697c2aa2c79453558b
Abhishek Saxena2 <abhisheksaxena2@M-C56R6F7MCW.local>
2024-06-24 23:44:35 +0530

    styles-added

--- src/components/video-card/index.tsx
+++ src/components/video-card/index.tsx
@@ -5,11 +5,20 @@ export const VideoCard=()=>{
     return(
         <div>
             <div className="grid grid-cols-12" >
-                <div className="col-span-1">
+                <div className="col-span-1 bg-red-500 justify-center">
                     <img className={"rounded-full w-40 h-40"} src="../../../public/unnamed.jpg" alt="dfvd" />
                 </div>
-                <div className="col-span-11">
+                
+                <div className="col-span-11 pl-5">
+                <div>
                     abjkdbvjkskv
+                </div>
+                <div className="col-span-11  text-gray-600 text-base">
+                    vdvndfklvndsklnvklds:vdsfjkvbndfk
+                </div>
+                <div className="col-span-11 text-gray-500 text-base">
+                    kljweaio | 13 days ago
+                </div>
                 </div>
             </div>
         </div>
